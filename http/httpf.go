9e60df96cc2aeceae23c71317dd8d2b2c0c6ad3e
shang <917058965@qq.com>
2019-06-20 10:18:15 +0800

    fix

--- http/httpf.go
+++ http/httpf.go
@@ -1,6 +1,9 @@
 package http
 
+import "net/http"
+
 type ApiEndPoint interface {
 	BindGET(api string, f interface{})
 	BindPOST(api string, f interface{})
+	BindResource(web string, handler http.Handler)
 }
